8fc0de58f63b4c88517fab63a5dc378ac11343bf
Art Gourieff <85128026+Gourieff@users.noreply.github.com>
2024-03-05 12:38:38 +0700

    UPDATE: ORT dependency for CU12
    +VersionUP (0.7.0 beta4)

--- scripts/reactor_version.py
+++ scripts/reactor_version.py
@@ -1,5 +1,5 @@
 app_title = "ReActor"
-version_flag = "v0.7.0-b3"
+version_flag = "v0.7.0-b4"
 
 from scripts.reactor_logger import logger, get_Run, set_Run
 from scripts.reactor_globals import DEVICE
